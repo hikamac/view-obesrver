8d0594a1fdbca9b55d2801889483b9ba4b19d5cb
hikamac <harimaru27@gmail.com>
2023-11-02 01:06:21 +0900

    add select mock

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -23,10 +23,32 @@ export const helloWorld = onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
-export const addMock = onRequest(async () => {
-  logger.info("Hello logs!", {structuredData: true});
-  const firestore: Firestore = admin.firestore();
-  const mockDock = {name: "John"};
-  const result = await firestore.collection("mock").add(mockDock);
-  console.log("result", result);
+export const addMock = onRequest(async (_, res) => {
+  try {
+    const firestore: Firestore = admin.firestore();
+    const mockDock = {name: "John", calledCount: 0};
+    const result = await firestore.collection("mock").add(mockDock);
+    res
+      .status(200)
+      .setHeader("Content-Type", "application/json")
+      .send(JSON.stringify(result));
+  } catch (err) {
+    res.status(500).setHeader("Content-Type", "text/plain").send(err);
+  }
+});
+
+export const selectMock = onRequest(async (_, res) => {
+  try {
+    const firestore: Firestore = admin.firestore();
+    const selected = await firestore
+      .collection("mock")
+      .where("name", "==", "John")
+      .get();
+    res
+      .status(200)
+      .setHeader("Content-Type", "application/json")
+      .send(JSON.stringify(selected.docs.map((doc) => doc.data())));
+  } catch (err) {
+    res.status(500).setHeader("Content-Type", "text/plain").send(err);
+  }
 });
