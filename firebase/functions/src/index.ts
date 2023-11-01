547e8eae24f94528c3f99b690f696eb0c85a9f20
hikamac <harimaru27@gmail.com>
2023-11-01 18:28:53 +0900

    mock add function

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -7,13 +7,26 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import { onRequest } from "firebase-functions/v2/https";
+import {onRequest} from "firebase-functions/v2/https";
+import * as admin from "firebase-admin";
 import * as logger from "firebase-functions/logger";
+import {Firestore} from "firebase-admin/firestore";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
+const options: admin.AppOptions = {};
+admin.initializeApp(options);
+
 export const helloWorld = onRequest((request, response) => {
-    logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+export const addMock = onRequest(async () => {
+  logger.info("Hello logs!", {structuredData: true});
+  const firestore: Firestore = admin.firestore();
+  const mockDock = {name: "John"};
+  const result = await firestore.collection("mock").add(mockDock);
+  console.log("result", result);
 });
