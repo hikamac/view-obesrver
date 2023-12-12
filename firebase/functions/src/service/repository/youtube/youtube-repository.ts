b05821c6f0e6824ba24f505399103ef96688ec0f
hikamac <harimaru27@gmail.com>
2023-12-12 17:33:06 +0900

    add youtube repository

---
+++ firebase/functions/src/service/repository/youtube/youtube-repository.ts
@@ -0,0 +1,37 @@
+import {defineString} from "firebase-functions/params";
+import {google} from "googleapis";
+import {VideoInfoItem} from "../../../model/youtube/video-info-item";
+
+export class YouTubeDataApiRepository {
+  protected youtube;
+
+  constructor() {
+    const apiKey = defineString("YOUTUBE_DATA_API_KEY");
+    this.youtube = google.youtube({version: "v3", auth: apiKey.value()});
+  }
+
+  public async listVideoInfo(
+    videoIds: string[],
+    part: Part[],
+  ): Promise<Array<VideoInfoItem>> {
+    const params = {part: part, id: videoIds};
+    const list = await this.youtube.videos.list(params);
+    const items = list.data.items != null ? list.data.items : null;
+    if (!items) {
+      return [];
+    }
+    return items as Array<VideoInfoItem>;
+  }
+}
+
+export type Part =
+  | "snippet"
+  | "contentDetails"
+  | "fileDetails"
+  | "player"
+  | "processingDetails"
+  | "recordingDetails"
+  | "statistics"
+  | "status"
+  | "suggestions"
+  | "topicDetails";
