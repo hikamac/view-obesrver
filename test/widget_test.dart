c9cdeb85661029495110e9999565e613e2ed4327
hikamac <harimaru27@gmail.com>
2024-02-25 02:06:30 +0900

    fix widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,24 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:view_observer/main.dart';
+import 'package:view_observer/views/organisms/news_list.dart';
+
+import 'firebase_test/mock.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  setupFirebaseAuthMocks();
+  setUpAll(() async => await Firebase.initializeApp());
+  testWidgets("NewsList test", (WidgetTester tester) async {
+    await tester.pumpWidget(const ProviderScope(
+      child: MyApp(),
+    ));
+    await tester.pumpAndSettle();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byType(NewsList), findsOneWidget);
   });
 }
