1310777588a0c23a5db079eff2c236130e76f97f
hikamac <harimaru27@gmail.com>
2024-07-01 19:25:41 +0900

    add debug logger

--- firebase/functions/src/controller/batches/fix-view-history-creted.batch.ts
+++ firebase/functions/src/controller/batches/fix-view-history-creted.batch.ts
@@ -1,19 +1,28 @@
 import * as logger from "firebase-functions/logger";
+
 import {onRequest} from "firebase-functions/v2/https";
 import {firestoreRegion} from "../../constant/setting-value";
 import {OkResponse} from "../../model/ok-response";
 import {ViewHistoryUseCase} from "../../service/usecases/view-history-usecase";
+import {DebugUtils} from "../debug/debug-utils";
 
 export const fixViewHistoryCreated = onRequest(
   {region: firestoreRegion},
   async (_, res) => {
+    // logger setup
+    const dutil = new DebugUtils();
+    dutil.setupLogFileGenerator();
     try {
       const viewHistoryUseCase = new ViewHistoryUseCase();
-      const result = viewHistoryUseCase.fixViewHistoryCreated();
+      const result = await viewHistoryUseCase.fixViewHistoryCreated();
+      console.log("controller");
       res.status(200).send(result);
     } catch (err) {
       logger.error(err);
+      console.error(err);
       res.status(500).send(OkResponse.NG);
+    } finally {
+      dutil.logFileEnd();
     }
   },
 );
