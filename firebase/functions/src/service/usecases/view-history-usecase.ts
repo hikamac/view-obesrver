5f8e11e29bf34ed3247a11def9099d4855996589
hikamac <harimaru27@gmail.com>
2024-08-20 18:33:36 +0900

    fix cursor name

--- firebase/functions/src/service/usecases/view-history-usecase.ts
+++ firebase/functions/src/service/usecases/view-history-usecase.ts
@@ -29,7 +29,7 @@ export class ViewHistoryUseCase {
 
     await this.debugRepo.setCursor(
       new DebugDocument({
-        lastDocId: fixResult.lastDocId,
+        viewHistoryDocId: fixResult.lastDocId,
         batchCount: fixResult.batchCount,
         totalFixed: fixResult.totalFixed,
       }),
