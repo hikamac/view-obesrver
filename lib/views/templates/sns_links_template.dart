b5f62c95aad0b7dd7c4f8fbda99f5386711bb0d0
hikamac <harimaru27@gmail.com>
2024-11-28 18:45:42 +0900

    sns link page scrollable

--- lib/views/templates/sns_links_template.dart
+++ lib/views/templates/sns_links_template.dart
@@ -11,14 +11,33 @@ class SNSLinksTemplate extends Template {
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
-    // double height = screenSize.height;
+    double height = screenSize.height;
     double width = screenSize.width;
     return Center(
-      child: SNSLinkList(
-        padding: EdgeInsets.symmetric(
-          horizontal: width * 0.2,
-      ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'SNS Links',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: height * 0.8,
+              child: SNSLinkList(
+                padding: EdgeInsets.symmetric(
+                  horizontal: width * 0.2,
+                ),
+              ),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
