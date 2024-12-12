6fa24b89b8a7c891e379eebdc77d8a5c24d8d970
hikamac <harimaru27@gmail.com>
2024-12-12 23:23:41 +0900

    main method

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,28 +1,28 @@
-// // This is a basic Flutter widget test.
-// //
-// // To perform an interaction with a widget in your test, use the WidgetTester
-// // utility in the flutter_test package. For example, you can send tap and scroll
-// // gestures. You can also use WidgetTester to find child widgets in the widget
-// // tree, read text, and verify that the values of widget properties are correct.
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
 
 // import 'package:firebase_core/firebase_core.dart';
 // import 'package:flutter_riverpod/flutter_riverpod.dart';
 // import 'package:flutter_test/flutter_test.dart';
 
 // import 'package:view_observer/main.dart';
-// // import 'package:view_observer/views/organisms/news_list.dart';
+// import 'package:view_observer/views/organisms/news_list.dart';
 
 // import 'firebase_test/mock.dart';
 
-// void main() {
-//   setupFirebaseAuthMocks();
-//   setUpAll(() async => await Firebase.initializeApp());
-//   testWidgets("NewsList test", (WidgetTester tester) async {
-//     await tester.pumpWidget(ProviderScope(
-//       child: MyApp(),
-//     ));
-//     await tester.pumpAndSettle();
+void main() {
+  // setupFirebaseAuthMocks();
+  // setUpAll(() async => await Firebase.initializeApp());
+  // testWidgets("NewsList test", (WidgetTester tester) async {
+  //   await tester.pumpWidget(ProviderScope(
+  //     child: MyApp(),
+  //   ));
+  //   await tester.pumpAndSettle();
 
-//     // expect(find.byType(NewsList), findsOneWidget);
-//   });
-// }
+  //   // expect(find.byType(NewsList), findsOneWidget);
+  // });
+}
